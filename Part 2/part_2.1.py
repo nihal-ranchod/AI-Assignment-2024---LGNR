65ae00b1025649c4c0a775d45be7a2b5af802fea
nihal-ranchod <nihal.ranchod@gmail.com>
2024-04-22 22:42:56 +0200

    Added: comments Changed: variable and function names for 2.1

--- Part 2/part_2.1.py
+++ Part 2/part_2.1.py
@@ -1,25 +1,25 @@
 import chess
 from reconchess.utilities import without_opponent_pieces, is_illegal_castle
 
-def moves(board):
-    moves = ['0000']
+def generate_possible_moves(board):
+    possible_moves = ['0000'] # Initialise with null move
 
     for move in board.pseudo_legal_moves:
-        moves.append(move.uci())
+        possible_moves.append(move.uci())
 
+    # Adding all valid castling moves in RBC
     for move in without_opponent_pieces(board).generate_castling_moves():
         if not is_illegal_castle(board, move) and move not in board.legal_moves:
-            # this would be a valid castling move in RBC
-            moves.append(move.uci())
+            possible_moves.append(move.uci())
 
-    moves.sort()
-    return moves
+    possible_moves.sort()
+    return possible_moves
 
 def main():
     fen_str = input()
     board = chess.Board(fen_str)
 
-    for move in moves(board):
+    for move in generate_possible_moves(board):
         print(move)
   
 if __name__ == "__main__":
