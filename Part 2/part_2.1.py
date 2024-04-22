28ebad502649a7bd86392f40fffd6072e434fef6
nihal-ranchod <nihal.ranchod@gmail.com>
2024-04-22 17:48:58 +0200

    fix sub 2 code, created separate functions

--- Part 2/part_2.1.py
+++ Part 2/part_2.1.py
@@ -1,23 +1,26 @@
 import chess
 from reconchess.utilities import without_opponent_pieces, is_illegal_castle
 
-def main():
-    fen_str = input()
-    board = chess.Board(fen_str)
+def moves(board):
     moves = ['0000']
+
+    for move in board.pseudo_legal_moves:
+        moves.append(move.uci())
 
     for move in without_opponent_pieces(board).generate_castling_moves():
         if not is_illegal_castle(board, move):
             # this would be a valid castling move in RBC
             moves.append(move.uci())
 
-    for move in board.pseudo_legal_moves:
-        moves.append(move.uci())
-
     moves.sort()
-    for i in moves:
-        print(i)
-  
+    return moves
 
+def main():
+    fen_str = input()
+    board = chess.Board(fen_str)
+
+    for move in moves(board):
+        print(move)
+  
 if __name__ == "__main__":
     main()
