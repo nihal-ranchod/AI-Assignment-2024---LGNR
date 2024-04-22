fe7c44c95ffcea7d08a4426aadd78f1f901ad307
nihal-ranchod <nihal.ranchod@gmail.com>
2024-04-22 17:33:42 +0200

    part 2 submission 3

---
+++ Part 2/part_2.3.py
@@ -0,0 +1,35 @@
+import chess
+from reconchess.utilities import without_opponent_pieces, is_illegal_castle
+
+def moves(board):
+    moves = ['0000']
+
+    for move in without_opponent_pieces(board).generate_castling_moves():
+        if not is_illegal_castle(board, move):
+            # this would be a valid castling move in RBC
+            moves.append(move.uci())
+
+    for move in board.pseudo_legal_moves:
+        moves.append(move.uci())
+
+    moves.sort()
+    return moves
+
+def main():
+    fen_str = input()
+    input_move = input()
+    board = chess.Board(fen_str)
+ 
+    next_states = []
+    for move in moves(board):
+        if move[2:4] == input_move:
+            board.push_uci(move)
+            next_states.append(board.fen())
+            board.pop()
+
+    next_states.sort()
+    for state in next_states:
+        print(state)
+
+if __name__ == "__main__":
+    main()
